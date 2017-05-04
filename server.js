32a66c105db970a434145053b02156110aa9da4d
Thirasan <thirasan@Thirasans-MacBook-Pro.local>
2017-05-04 15:33:09 +0700

    book

--- server.js
+++ server.js
@@ -19,27 +19,26 @@ var mysql_conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'CSV_DB'
+    database: 'Library'
 });
 
 app.listen(3943)
 
 var table = "ready";
 
-app.post('/', jsonParser, function (req, res) {
+app.get('/app', jsonParser, function (req, res) {
     name = req.body.firstName;
-        console.log("one");
-
-    var query = mysql_conn.query('SELECT * FROM author WHERE Name = "'+name +'"', function(err, result){
-        res.json(result);
+    console.log(name);
+    console.log("sure");
+    var query = mysql_conn.query('SELECT * FROM book', function(err, result){
         table = result;
+        res.json(result);
     });
     query
     .on('error', function(err) {
         // Handle error, an 'end' event will be emitted after this as well
     })
     .on('result', function(row) {
-        console.log("three");
         // Pausing the connnection is useful if your processing involves I/O
         mysql_conn.pause();
         
@@ -49,14 +48,40 @@ app.post('/', jsonParser, function (req, res) {
     })
     .on('end', function() {
         // all rows have been received
-        console.log("two");
         console.log('end after mysql');
     });
     function processRow(row,callback){
-        console.log(row);
         callback();
     } 
 })
+
+app.get('/book/:name', jsonParser, (req ,res) => {
+    const name = req.params.name
+    var query = mysql_conn.query('SELECT * FROM book WHERE Book_Name="' + name + '"', function(err, result){
+        table = result;
+        res.json(result);
+    });
+    query
+    .on('error', function(err) {
+        // Handle error, an 'end' event will be emitted after this as well
+    })
+    .on('result', function(row) {
+        // Pausing the connnection is useful if your processing involves I/O
+        mysql_conn.pause();
+        
+        processRow(row, function() {
+        mysql_conn.resume();
+        });
+    })
+    .on('end', function() {
+        // all rows have been received
+        console.log('end after mysql');
+    });
+    function processRow(row,callback){
+        callback();
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.json(table);
